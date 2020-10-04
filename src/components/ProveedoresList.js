8987e099fd79a7fba123c689fee53a836b0f7034
Jeanine Gill <jeanine.sanchez@upa.edu.py>
2020-10-04 13:01:19 -0300

    lista de proveedores

---
+++ src/components/ProveedoresList.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+function ProveedoresList(props) {
+    return (
+        <p>
+            lista de proveedores
+        </p>
+    )
+}
+export default ProveedoresList;
