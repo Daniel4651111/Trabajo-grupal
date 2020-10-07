359a48ef1f8310f9e783a88078215aca2ba57da2
ZileidyAB <zileidy.arguello@upa.edu.py>
2020-10-06 23:12:57 -0300

    Mejora de empresa

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import { Layout, Menu } from 'antd';
 import "antd/dist/antd.css";
@@ -15,6 +15,8 @@ import Contactos from './components/Contactos';
 import About from './components/About';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { AppstoreOutlined, SearchOutlined } from '@ant-design/icons';
+import axios from 'axios';
+import moment from 'moment';
 
 
 
@@ -25,7 +27,7 @@ function AppMenu() {
   const [current, setCurrent] = useState()
   return (
 
-// DEFINICION DE LA ESTRUCTURA DEL MENU PRINCIPAL - BARRA DE ARRIBA
+    // DEFINICION DE LA ESTRUCTURA DEL MENU PRINCIPAL - BARRA DE ARRIBA
     <Menu onClick={(value) => setCurrent(value)} selectedKeys={[current]} mode="horizontal">
       <Menu.Item key="home" icon={<AppstoreOutlined />}>
         <Link to="/">Home</Link>
@@ -41,14 +43,14 @@ function AppMenu() {
         <Menu.Item key="productos:2"><Link to="/productos/new">Nuevo Producto</Link></Menu.Item>
       </SubMenu>
 
-{/* categoria */}
+      {/* categoria */}
       <SubMenu key="categorias" icon={<AppstoreOutlined />} title="Categorías">
         <Menu.Item key="categorias:1"><Link to="/categorias">Categorías</Link></Menu.Item>
         <Menu.Item key="categorias:2"><Link to="/categorias/new">Nueva Categoría</Link></Menu.Item>
       </SubMenu>
 
-{/* proveedores*/}
-<SubMenu key="proveedores" icon={<AppstoreOutlined />} title="Proveedores">
+      {/* proveedores*/}
+      <SubMenu key="proveedores" icon={<AppstoreOutlined />} title="Proveedores">
         <Menu.Item key="proveedores:1"><Link to="/proveedores">Lista de proveedores</Link></Menu.Item>
       </SubMenu>
 
@@ -122,38 +124,61 @@ function ProveedoresRoutes(props) {
 }
 
 //ESTRUCTURA DE LA PÁGINA EN SÍ
-function App() {
+function App(props) {
+  const [empresas, setEmpresas] = useState([]);
+
+  const getEmpresas = () => {
+    // axios.get('ws/rest/productos/paginated', { params: { pageSize: 2, first: 0 }})
+    axios.get('proyecto_upa/rest/empresas') //Peticiones al Backend
+      .then(res => {
+        setEmpresas(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  useEffect(() => {
+    getEmpresas();
+  }, [])
+
   return (
     <Router>
-      <Layout>
-        <Header style={{ color: 'white', fontSize: 30, textAlign: 'center' }}>
-          <div>Das Kaufhaus</div>
-        </Header>
+      {empresas.map(empresa => {
+        return (          
+          <Layout>
+            
+            <Header style={{ color: 'white', fontSize: 30, textAlign: 'center' }}>
+              <div>{empresa.nombre}</div>
+            </Header>
 
-        <Content>
-          <div className="site-layout-content">
-            <AppMenu />
-            <br />
-            {/* Secccion donde se van a mostrar los diferentes componentes que rendericemos */}
-            <>
-              <Route path="/" exact component={Home} />
-              <Route path="/productos" component={ProductosRoutes} />
-              <Route path="/categorias" component={CategoriasRoutes} />
-              <Route path="/proveedores" component={ProveedoresRoutes} />
-              <Route path="/contactos" component={Contactos} />
-              <Route path="/about" component={About} />
-              {/* Hacemos esto porque tasks tiene subrutas */}
-              {/* <Route path="/tasks" component={TaskRoutes} /> */}
-            </>
-          </div>
-        </Content>
+            <Content>
+              <div className="site-layout-content">
+                <AppMenu />
+                <br />
+                {/* Secccion donde se van a mostrar los diferentes componentes que rendericemos */}
+                <>
+                  <Route path="/" exact component={Home} />
+                  <Route path="/productos" component={ProductosRoutes} />
+                  <Route path="/categorias" component={CategoriasRoutes} />
+                  <Route path="/proveedores" component={ProveedoresRoutes} />
+                  <Route path="/contactos" component={Contactos} />
+                  <Route path="/about" component={About} />
+                  {/* Hacemos esto porque tasks tiene subrutas */}
+                  {/* <Route path="/tasks" component={TaskRoutes} /> */}
+                </>
+              </div>
+            </Content>
 
-        <Footer style={{ textAlign: 'center' }}>
-          <p>0981-123-456</p>
-          <p>das_kaufhaus@gmail.com</p>
-          <p>Asuncion - Paraguay</p>
-        </Footer>
-      </Layout>
+            <Footer style={{ textAlign: 'center' }}>
+              <p>{empresa.telefono}</p>
+              <p>{empresa.email}</p>
+              <p>{empresa.ciudad} - {empresa.pais}</p>
+            </Footer>
+
+          </Layout>
+        )
+      })}
     </Router>
 
   );
